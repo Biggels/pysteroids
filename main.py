a41036bde0ed333880a600b0bdef0603824a5717
Ray <haackra@gmail.com>
2026-02-04 12:20:39 -0500

    Refactor game loop with sprite groups
    Introduce `updatable` and `drawable` sprite groups to manage game
    objects more effectively. This change centralizes the update and drawing
    logic for all sprites, making the main game loop cleaner and more
    scalable.

--- main.py
+++ main.py
@@ -11,19 +11,34 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
+
+        # process events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # update
+        updatable.update(dt)
+
+        # draw
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
+
+        # clock
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
